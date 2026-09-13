1bdb8385b30a306ae704cf9ae18e8be360dbcc30
Anthony <akingori371@gmail.com>
2026-09-13 21:39:56 +0300

    import random method and store it in a variable number

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import random
 que = int(input("Kindly guess a number btw the range of 1-100"))
 
 if que > 100 :
     print("Too High")
 elif que < 100:
     print("Too low")
+number = random.randint(1,100)
+
+print(number)
 
